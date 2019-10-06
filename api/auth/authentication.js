5fb78461bf9599bf909bfdab6c86f58a695e1d9a
Shiben Dutta <fit@qlik.com>
2019-10-06 13:15:59 +0530

    counters function updated

--- api/auth/authentication.js
+++ api/auth/authentication.js
@@ -12,41 +12,20 @@ authMethods = {
 
 dummyAuth: (req, res, next) =>{
   console.log('auth success');
-  req.body['currentUser'] = "Super Man";
+  // execute function to authenticate/authorize the user
+  req.body['currentUser'] = "Super Man"; /**Add current user to request body */
+  req.body['currentUserRole'] = "admin"; /**Add current user role to request body */
   next();
 },
 
-authCheck: (req, res, next) => {
-  const promise1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if(req.headers.authorization === 'admin') {
-        resolve({auth: true, role: 'admin'});
-      }else {
-        reject('non-admin');
+userRole: (req, res, next) =>{
+  setTimeout(() => {
+    if(req.headers.authorization === 'admin') {
+      next();
+    }else {
+      return 'non-admin';
       }
-    }, 3000);
-  });
-
-  promise1.then(value => {
-    console.log(value);
-    next();
-    }).catch((err) => {
-      res.status(500).json({
-        result: 'Could not resolve authentiaction',
-        message: err.message
-      });
-    })
-  },
-
-  userRole: (req, res, next) =>{
-    setTimeout(() => {
-      if(req.headers.authorization === 'admin') {
-        next();
-      }else {
-        return 'non-admin';
-      }
-    }, 3000);
-    
+    }, 3000);  
   }
 };
 
--- api/response/counters.js
+++ api/response/counters.js
@@ -13,7 +13,7 @@ router.get("/all", (req, res, next) => {
   });
 });
 
-router.post("/addCounter", (req, res, next) => {
+router.post("/addCounter/FFR", (req, res, next) => {
   const CTR = new Counters({
     _id: new mongoose.Types.ObjectId(),
     modelType: "FFR",
@@ -31,8 +31,80 @@ router.post("/addCounter", (req, res, next) => {
   });
 });
 
-router.delete("/delete/:_id", (req, res, next) =>{
+router.post("/addCounter/BFR", (req, res, next) => {
+  const BFR = new Counters({
+    _id: new mongoose.Types.ObjectId(),
+    modelType: "BFR",
+    sequence_value: 1,
+  });
+  BFR.save().then((result) =>{
+    res.status(201).json({
+      result: result
+    });
+  })
+  .catch((err) =>{
+    res.status(500).json({
+      result: err
+    });
+  });
+});
 
+router.post("/addCounter/NPR", (req, res, next) => {
+  const NPR = new Counters({
+    _id: new mongoose.Types.ObjectId(),
+    modelType: "NPR",
+    sequence_value: 1,
+  });
+  NPR.save().then((result) =>{
+    res.status(201).json({
+      result: result
+    });
+  })
+  .catch((err) =>{
+    res.status(500).json({
+      result: err
+    });
+  });
+});
+
+router.post("/addCounter/SPR", (req, res, next) => {
+  const CTR = new Counters({
+    _id: new mongoose.Types.ObjectId(),
+    modelType: "SPR",
+    sequence_value: 1,
+  });
+  SPR.save().then((result) =>{
+    res.status(201).json({
+      result: result
+    });
+  })
+  .catch((err) =>{
+    res.status(500).json({
+      result: err
+    });
+  });
+});
+
+router.post("/addCounter/EPC", (req, res, next) => {
+  const EPC = new Counters({
+    _id: new mongoose.Types.ObjectId(),
+    modelType: "FFR",
+    sequence_value: 1,
+  });
+  EPC.save().then((result) =>{
+    res.status(201).json({
+      result: result
+    });
+  })
+  .catch((err) =>{
+    res.status(500).json({
+      result: err
+    });
+  });
+});
+
+router.delete("/delete/:_id", (req, res, next) =>{
+  
 });
 
 module.exports = router;
